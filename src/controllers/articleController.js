8ff01267b7293a3e84c0a123fa4ee09fb0f3feb5
nikko febika <febika.nikko@gmail.com>
2021-06-22 23:40:02 +0700

    fix api

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -1,13 +1,30 @@
 const articleModel = require("../models/articleModel");
 exports.getAll = async (req, res, next) => {
-    await articleModel.getAll().then(result => {
-        res.status(200).json({
-            data: result
-        })
-    }).catch(error => {
-        const err = new Error('Error articleController getAll :');
-        err.errorStatus = 501;
-        err.data = error;
-        throw next(err);
+  await articleModel
+    .getAll()
+    .then((result) => {
+      res.status(200).json({
+        data: result,
+      });
     })
-}
+    .catch((error) => {
+      const err = new Error("Error articleController getAll :");
+      err.errorStatus = 501;
+      err.data = error;
+      throw next(err);
+    });
+};
+
+exports.getBySeoTitle = async (req, res, next) => {
+  await articleModel
+    .getBySeoTitle(req.params.seo_title)
+    .then((result) => {
+      res.status(200).json({ data: result });
+    })
+    .catch((error) => {
+      const err = new Error("Error articleController getBySeoTitle :");
+      err.errorStatus = 501;
+      err.data = error;
+      throw next(err);
+    });
+};
--- src/routes/articlesRoutes.js
+++ src/routes/articlesRoutes.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const articleController = require("../controllers/articleController");
 
-router.get('/', articleController.getAll);
+router.get("/", articleController.getAll);
+router.get("/get_by_seotitle/:seo_title", articleController.getBySeoTitle);
 
 module.exports = router;
